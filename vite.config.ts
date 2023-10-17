639af8a9c8bdbbf8e37c3fc1bd5aa618a80f154b
zhaojie09 <zhaojie09@kuaishou.com>
2023-10-17 18:36:10 +0800

    feat: 🎸 打包增加类型声明文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import vue from '@vitejs/plugin-vue';
-import viteCompression from 'vite-plugin-compression';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig(() => ({
   plugins: [
     vue(),
-    viteCompression(),
+    dts({
+      rollupTypes: true,
+    }),
   ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'vue-useRequest',
+      fileName: 'index',
     },
     rollupOptions: {
       external: ['vue', 'vue-demi'],
